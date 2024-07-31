b445328b60675de861bcd374ea2d7cdbed8d83d7
candyap <candyap182@icloud.com>
2024-08-01 04:03:36 +0700

    Initial Commit

---
+++ TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by Candy Pangestu on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TunnelOfCirclesTests/TunnelOfCirclesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesTests.swift
+//  TunnelOfCirclesTests
+//
+//  Created by Candy Pangestu on 8/1/24.
+//
+
+import Testing
+@testable import TunnelOfCircles
+
+struct TunnelOfCirclesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
